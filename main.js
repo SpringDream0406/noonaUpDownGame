5fe9668660880e5e198b560cc3d170221e765e13
SpringDream0406 <SpringDream0406@gmail.com>
2024-02-11 18:22:15 +0900

    updown게임 영상대로

--- main.js
+++ main.js
@@ -12,8 +12,19 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 10;
+let gameOver = false;
+let chanceArea = document.getElementById("change-area");
+let history = [];
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+
+// focus, 한번만 쓰일 함수는 이름 지정안하고 그냥 안에 집어넣는것도 방법
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100 + 1);
@@ -21,18 +32,54 @@ function pickRandomNum() {
 }
 
 function play() {
-    let userValue = userInput.value;
-    if (userValue < computerNum){
-        console.log("Up!!");
-        resultArea.textContent = "Up!!!"
-    } else if (userValue > computerNum) {
-        console.log("Down!!");
-        resultArea.textContent = "Down!!!"
-    } else {
-        console.log("정답!");
-        resultArea.textContent = "정답!"
+  let userValue = userInput.value;
 
-    }
+  if (userValue < 1 || userValue > 100) {
+    resultArea.textContent = "1과 100사이 숫자를 입력해주세요.";
+    return;
+  }
+
+  if (history.includes(userValue)) {
+    resultArea.textContent =
+      "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.";
+    return;
+  }
+
+  chances--;
+  chanceArea.textContent = `남은기회: ${chances}번`;
+  console.log("change", chances);
+
+  if (userValue < computerNum) {
+    console.log("Up!!");
+    resultArea.textContent = `${userValue}보다 큰 수 입니다. Up!!`;
+  } else if (userValue > computerNum) {
+    console.log("Down!!");
+    resultArea.textContent = `${userValue}보다 작은 수 입니다. Down!!`;
+  } else {
+    console.log("정답!");
+    resultArea.textContent = `${userValue} 정답입니다.`;
+    gameOver = true;
+  }
+
+  history.push(userValue);
+  console.log(history);
+
+  if (chances == 0) {
+    gameOver = true;
+  }
+
+  if (gameOver) {
+    playButton.disabled = true;
+  }
+}
+
+function reset() {
+  // user input창이 깨끗하게 정리되고
+  userInput.value = "";
+  // 새로운 번호가 생성되고
+  pickRandomNum();
+
+  resultArea.textContent = "1~100의 숫자를 입력해주세요.";
 }
 
 pickRandomNum();
